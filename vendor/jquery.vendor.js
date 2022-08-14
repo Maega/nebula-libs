657053bd8c5b9133f818de70975d87f5e2a33552
Tristan Gauci <tristan@maega.com.au>
2022-08-14 22:30:25 +1000

    Added jQuery Import Helper

---
+++ vendor/jquery.vendor.js
@@ -0,0 +1,3 @@
+// Import jQuery local to this module and export it without global variables so we can freely import it in other modules
+import 'https://code.jquery.com/jquery-3.6.0.min.js'
+export default window.jQuery.noConflict(true)
